144115a43d3407eb7a06f69ba995a8ed6ecc722d
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-09-05 10:01:58 -0500

    Algun cambio

--- index-post.php
+++ index-post.php
@@ -1,18 +1,3 @@
-<!-- <?php
-include("conexion.php");
-$con = conexion();
-
-$doc = $_POST["doc"];
-$nom = $_POST["nom"];
-$ape = $_POST["ape"];
-$dir = $_POST["dir"];
-$cel = $_POST["cel"];
-
-$sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
-pg_query($con, $sql);
-
-header("location:index.php");
-?> -->
 
 <?php
 include("conexion.php");

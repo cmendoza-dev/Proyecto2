cc06a05858d13f1c8363229ce72e2142295945c5
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-09-05 09:53:58 -0500

    Algun cambio

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "containers-us-west-203.railway.app";
+	$port = "6958";
+	$dbname = "railway";
+	$user = "postgres";
+	$password = "4MztO8M0VkUDlbOC3hnr";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 

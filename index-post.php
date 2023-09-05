fb671bd6d9ca3a77ef083a4d36c40a0efec87790
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-09-05 09:59:13 -0500

    Algun cambio

--- index-post.php
+++ index-post.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 include("conexion.php");
 $con = conexion();
 
@@ -12,4 +12,29 @@ $sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
 pg_query($con, $sql);
 
 header("location:index.php");
+?> -->
+
+<?php
+include("conexion.php");
+$con = conexion();
+
+$doc = $_POST["doc"];
+$nom = $_POST["nom"];
+$ape = $_POST["ape"];
+$dir = $_POST["dir"];
+$cel = $_POST["cel"];
+
+// Use prepared statement
+$sql = "INSERT INTO persona VALUES(default, $1, $2, $3, $4, $5)";
+$result = pg_query_params($con, $sql, array($doc, $nom, $ape, $dir, $cel));
+
+if ($result) {
+    header("location:index.php");
+} else {
+    // Handle the error, for example:
+    echo "Error: " . pg_last_error($con);
+}
+
+// Close the database connection
+pg_close($con);
 ?>

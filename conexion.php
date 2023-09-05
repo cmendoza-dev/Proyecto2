5d6ec9245106542bc764a0a0837344cfd1aa73f9
MasterCard31 <carlos.mendoza.s@tecsup.edu.pe>
2023-09-05 10:07:21 -0500

    Algun cambio

--- conexion.php
+++ conexion.php
@@ -1,28 +1,16 @@
-
 <?php
 
-function conexion(){
+	function conexion(){
 
-    $host = "containers-us-west-203.railway.app";
-    $port = "6958";
-    $dbname = "railway";
-    $user = "postgres";
-    $password = "4MztO8M0VkUDlbOC3hnr";
+	$host = "containers-us-west-203.railway.app";
+	$port = "6958";
+	$dbname = "railway";
+	$user = "postgres";
+	$password = "4MztO8M0VkUDlbOC3hnr";
 
-    // Use pg_connect with proper connection string
-    $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
-    
-    $db = pg_connect($connection_string);
+	$db = pg_connect("$host $port $dbname $user $password");
 
-    if (!$db) {
-        die("Error in connection: " . pg_last_error());
-    }
-
-    return $db;
+	return $db;
 }
-
-// Example usage:
-$con = conexion();
-
-// Now you can use $con to perform database operations
 ?>
+
